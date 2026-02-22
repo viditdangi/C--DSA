a3499ed12f87712d96ff54b8d170ad3eb289c4b0
viditdangi <viditdangi18@gmail.com>
2026-02-22 22:48:31 +0530

    notes

---
+++ LeetCode/11.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int maxArea(vector<int>& height) {
+    int l = 0, r = height.size() - 1;
+    int maxWater = 0;
+
+    while (l < r) {
+        int h = min(height[l], height[r]);
+        int width = r - l;
+        maxWater = max(maxWater, h * width);
+        if (height[l] < height[r])
+            l++;
+        else
+            r--;
+    }
+
+    return maxWater;
+}
+int main() {
+    vector<int> height = {1,8,6,2,5,4,8,3,7,7};
+    cout << maxArea(height) << endl;
+    return 0;
+}
