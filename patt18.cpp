2a6c9e395c6559c21c92e1869d28f98b1f98d85d
viditdangi <viditdangi18@gmail.com>
2026-03-01 21:35:51 +0530

    notes

---
+++ patt18.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        //spaces
+         for (int j=1;j<=n-i;j++){
+         cout<<" ";
+         }
+         for(int j=1;j<=i;j++){
+            cout<<"*";
+         }
+      cout<<endl;   
+    }
+    for(int i=n-1;i>=1;i--){
+        //spaces
+         for (int j=1;j<=n-i;j++){
+         cout<<" ";
+         }
+         for(int j=1;j<=i;j++){
+            cout<<"*";
+         }
+      cout<<endl;   
+    }
+    return 0;
+}
