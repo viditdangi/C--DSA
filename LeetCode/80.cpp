27b687689cc270c0996f19ecde1d107c75702ff0
viditdangi <viditdangi18@gmail.com>
2026-03-14 00:17:31 +0530

    notes

--- LeetCode/80.cpp
+++ LeetCode/80.cpp
@@ -3,17 +3,14 @@
 using namespace std;
 
 int removeDuplicates(vector<int>& nums) {
-    if (nums.empty()) return 0;
-    
-    int i = 0;
-    for (int j = 1; j < nums.size(); j++) {
-        if (i == 0 || i == 1 || nums[j] != nums[i - 1]) {
-            nums[++i] = nums[j];
+    int j = 1;
+    for (int i = 1; i < nums.size(); i++) {
+        if (j == 1 || nums[i] != nums[j - 2]) {
+            nums[j++] = nums[i];
         }
     }
-    return i + 1;
+    return j;
 }
-
 int main() {
     vector<int> nums = {1,1,1,2,2,3};
     int k = removeDuplicates(nums);
