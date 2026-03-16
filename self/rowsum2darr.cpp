6ea39fe5b8eaef89db8d9c58bd07f0981ad96552
viditdangi <viditdangi18@gmail.com>
2026-03-16 23:41:17 +0530

    notes

---
+++ self/rowsum2darr.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+void printSum(int arr [][3],int i ,int j){
+for (int i=0; i<3; i++){
+int sum = 0;
+for (int j=0; j<3; j++) {
+    sum += arr[i][j];
+}
+cout<<sum<<" ";
+}}
+int main(){
+int arr[3][3];
+for(int i=0;i<3;i++){
+   for(int j=0;j<3;j++) {
+    cin>>arr[i][j]; 
+   }
+}
+    printSum(arr,3,3);
+}
